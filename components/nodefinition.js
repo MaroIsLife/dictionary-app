263541889fb63e36f6583fe8d4e0969d34e1b9a3
MaroIsLife <ougnou10@gmail.com>
2023-03-31 09:08:01 +0000

    Fixed Responsive

--- components/nodefinition.js
+++ components/nodefinition.js
@@ -6,15 +6,14 @@ const Nodefinition = ({word}) =>
 
 	return (
 		<>
-		<div className="flex flex-col items-center mt-32">
+			<div className="flex flex-col items-center mt-32">
 			<HiOutlineEmojiSad className="mb-4" size={140} color="AC67EE"></HiOutlineEmojiSad>
 		<h1 className="text-2xl font-bold">No Definition{/* for <span className="text-purple-500 italic ">{word}</span>*/}</h1>
 		<div className="mt-4">
-			<p className="text-gray-500 text-lg">Sorry we could not find the word you're looking for {/*<span className="text-purple-500 font-bold ">{word}</span> */}</p>
 
-		</div>
-			
-		<p className="text-gray-500 text-lg">in our dictionary,&nbsp; please try searching another word.</p>
+		<p className="text-gray-500 w-full text-lg mx-3 ">Sorry we could not find the word you're looking for {/*<span className="text-purple-500 font-bold ">{word}</span> */}</p>
+		<p className="text-gray-500 w-full text-lg mx-3">in our dictionary,&nbsp; please try searching another word.</p>
+		</div>	
 
 		</div>
 		</>
